87977ca00ecb0537923c0c67a95aadc66019c0bd
james@sweetelectricity.com <james@jamesawhite.com>
2020-04-10 10:16:35 +0200

    Int: Added core utilities

---
+++ lib/util/core_utilities.dart
@@ -0,0 +1,190 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:crypto/crypto.dart';
+import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
+
+class CoreUtilities {
+  static int logCounter = 0;
+
+  static void logTiming(String item, DateTime startTime) {
+    logCounter++;
+    final Duration d = DateTime.now().difference(startTime);
+    print((d.inMilliseconds / 1000.0).toString() + ' ($logCounter): ' + item);
+  }
+
+  static String generateToken(String userId, String procName, {String paramString = ''}) {
+    final Duration difference = DateTime.now().toUtc().difference(DateTime.utc(1993, 7, 25, 15, 0, 0));
+    //final int timeBlocks = (difference.inSeconds / 5760).toInt();
+    final int timeBlocks = difference.inSeconds ~/ 5760;
+    if (paramString.isNotEmpty) {
+      paramString = '#' + paramString;
+    }
+    final String accessString = '${userId.toUpperCase()}#$procName#${timeBlocks.toString()}$paramString';
+    final List<int> bytes = utf8.encode(accessString); // data being hashed
+    final Digest digest = sha256.convert(bytes);
+    return '$digest'.toUpperCase();
+  }
+
+  static String getFormattedMoney(num amount, num decimalPlaces, String currencySymbol) {
+    String finalStr = '';
+    if (amount != null) {
+      String formatDecimals = '#####0.00';
+      switch (decimalPlaces) {
+        case 0:
+          formatDecimals = '#####0';
+          break;
+        case 1:
+          formatDecimals = '#####0.0';
+          break;
+        case 2:
+          formatDecimals = '#####0.00';
+          break;
+        case 3:
+          formatDecimals = '#####0.000';
+          break;
+        case 4:
+          formatDecimals = '#####0.0000';
+          break;
+        default:
+          formatDecimals = '#####0.00';
+          break;
+      }
+
+      final String amountStr = NumberFormat(formatDecimals).format(amount);
+
+      finalStr = currencySymbol.replaceAll('^', amountStr);
+    }
+
+    return finalStr;
+  }
+
+  static Widget elegantDivider(String text, num topPadding, num bottomPadding) {
+    return Padding(
+      padding: EdgeInsets.only(top: topPadding, bottom: bottomPadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.white10,
+                    Colors.white,
+                  ],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 1.0),
+                  stops: <double>[0.0, 1.0],
+                  tileMode: TileMode.clamp),
+            ),
+            width: 100.0,
+            height: 1.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white, fontSize: 16.0, fontFamily: 'WorkSansMedium'),
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.white,
+                    Colors.white10,
+                  ],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 1.0),
+                  stops: <double>[0.0, 1.0],
+                  tileMode: TileMode.clamp),
+            ),
+            width: 100.0,
+            height: 1.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  static void showInSnackBar(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey, String value, {int durationInSeconds = 3}) {
+    FocusScope.of(context).requestFocus(FocusNode());
+    scaffoldKey.currentState?.removeCurrentSnackBar();
+    scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(
+        value,
+        textAlign: TextAlign.center,
+        style: const TextStyle(color: Colors.white, fontSize: 16.0, fontFamily: 'WorkSansSemiBold'),
+      ),
+      backgroundColor: Colors.blue,
+      duration: Duration(seconds: durationInSeconds),
+    ));
+  }
+
+  static num unInt(num n) {
+    if (n == n.toInt()) {
+      n += 0.00000001;
+    }
+    return n;
+  }
+
+    static Widget styleForDisabled(Widget w, {num borderRadius = 0.0}) {
+    return Container(
+      foregroundDecoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: Colors.grey,
+        backgroundBlendMode: BlendMode.lighten,
+      ),
+      child: Container(
+        foregroundDecoration: const BoxDecoration(
+          color: Colors.grey,
+          backgroundBlendMode: BlendMode.saturation,
+        ),
+        child: w,
+      ),
+    );
+  }
+
+  static Future<bool> showAlert(BuildContext context, String title, String body, String buttonText, {bool showCancelButton = false, String cancelButtonText = 'Cancel'}) async {
+    return showDialog<bool>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  body,
+                  textAlign: TextAlign.justify,
+                  style: const TextStyle(fontFamily: 'AvenirNextRegular', fontStyle: FontStyle.normal, fontSize: 16.0, height: 1.0),
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            showCancelButton == true
+                ? FlatButton(
+                    child: Text(cancelButtonText),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  )
+                : Container(),
+            FlatButton(
+              child: Text(buttonText),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
