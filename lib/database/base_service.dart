790299de907e9854ff44e94e2aae356b5d9ab851
james@sweetelectricity.com <james@jamesawhite.com>
2020-05-10 12:07:29 +0200

    1.0.5: enhanced error handling

--- lib/database/base_service.dart
+++ lib/database/base_service.dart
@@ -8,9 +8,8 @@ import 'package:sqflite/sqflite.dart';
 class BaseModel {
   BaseModel();
   factory BaseModel.fromJson() => null;
-  Map<String,dynamic> toJson() => null;
+  Map<String, dynamic> toJson() => null;
 }
-
 
 class BaseTableHelper {
   BaseTableHelper();
@@ -34,9 +33,7 @@ mixin BaseFields {
 }
 
 class BaseService {
-  
   Future<List<BaseModel>> selectAllFromLocalDb(Database db, BaseTableHelper tableHelper, String tableName) async {
-
     final List<Map<String, dynamic>> result = await db.query(tableName);
 
     final List<BaseModel> records = <BaseModel>[];
@@ -52,14 +49,13 @@ class BaseService {
     return records;
   }
 
-  Future<num> getLastUpdatedTime(Database db, BaseTableHelper tableHelper,String tableName,String colUpdatedAtValue) async {
-    
+  Future<num> getLastUpdatedTime(Database db, BaseTableHelper tableHelper, String tableName, String colUpdatedAtValue) async {
     final List<Map<String, dynamic>> table = await db.rawQuery('SELECT MAX($colUpdatedAtValue) AS maxDate FROM $tableName');
     final num timeValue = table.first['maxDate'];
     return timeValue;
   }
 
-  Future<void> clearTable(Database db, BaseTableHelper tableHelper,String tableName) async {
+  Future<void> clearTable(Database db, BaseTableHelper tableHelper, String tableName) async {
     final String query = 'DELETE FROM $tableName';
     await db.rawDelete(query).then((void dummy) {
       //setIntPrefStrKey(LAST_CACHE_CLEAR_KEY + tableHelper.getTableName(tableType), DateTime.now().millisecondsSinceEpoch);
@@ -72,7 +68,7 @@ class BaseService {
     int deletedCounter = 0;
 
     bool doNormalizeMap;
-    
+
     final List<dynamic> jsonResultSets = json.decode(rawResults);
     print('$tableName result sets received from cloud = ${jsonResultSets.length}');
 
@@ -90,20 +86,16 @@ class BaseService {
           doNormalizeMap = testMap.length != jsonItem.length;
           if (doNormalizeMap) {
             print('Normalize map called for $tableName, # of fields on the wire = ${jsonItem.length}, # of fields in internal DB = ${testMap.length}');
-            for (int i = 0; i < jsonItem.length; i++)
-            {
+            for (int i = 0; i < jsonItem.length; i++) {
               final String key = jsonItem.keys.elementAt(i);
-              if (!testMap.containsKey(key))
-              {
+              if (!testMap.containsKey(key)) {
                 print('$key field is on the wire but not in the internal database');
               }
             }
 
-            for (int i = 0; i < testMap.length; i++)
-            {
+            for (int i = 0; i < testMap.length; i++) {
               final String key = testMap.keys.elementAt(i);
-              if (!jsonItem.containsKey(key))
-              {
+              if (!jsonItem.containsKey(key)) {
                 print('$key field is in the internal database but not on the wire');
               }
             }
@@ -118,8 +110,7 @@ class BaseService {
         }
 
         // important: make sure to normalize the map before adding the updatedAtValue!
-        if (doNormalizeMap)
-        {
+        if (doNormalizeMap) {
           jsonItem = tableHelper.normalizeMap(jsonItem);
         }
 
@@ -148,7 +139,6 @@ class BaseService {
             });
           }
         } else {
-          
           if ((table != null) && (table.isNotEmpty)) {
             final String rowId = table.first['id'].toString();
             if ((rowId != null) && (rowId.isNotEmpty)) {
@@ -167,7 +157,7 @@ class BaseService {
   }
 
   Future<List<dynamic>> updateSqlTablesFromJson(String jsonResults, List<BaseTableHelper> tables, Database db, dynamic appDomainType, {Function informUser}) async {
-    List<dynamic> adHocData;
+    List<dynamic> adHocData = <dynamic>[]; // prepare to return an empty list instead of null
 
     if (jsonResults.startsWith('[[')) {
       jsonResults = jsonResults.substring(1, jsonResults.length - 1);
@@ -203,12 +193,22 @@ class BaseService {
         print('server messages received');
       }
 
-      if (!isProcessed)
-      {
+      if (!isProcessed) {
         print('The following data was not inserted into the device DB');
         print('It is required that the primary key for the table to be the first field in the JSON data. Please check the JSON data format.');
         print(ms);
-        assert(false);
+        if (ms.startsWith(r'[{"errorId"')) {
+          final List<dynamic> errorItems = jsonDecode('$ms');
+          if ((errorItems != null) && (errorItems.isNotEmpty)) {
+            adHocData = errorItems;
+          }
+          print('server messages received');
+        } else {
+          // There is a chance that the server returned data that this version
+          // of the software is not expecting, such as in cases when new features
+          // have been added to new releases and this is an older release
+          // in these cases, just ignore the extra data
+        }
       }
     }
     return adHocData;
