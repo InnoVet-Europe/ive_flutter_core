ef06ad935d79991bcad62c8c945d39e9eeae330f
james@sweetelectricity.com <james@jamesawhite.com>
2020-05-11 08:12:11 +0200

    1.0.7: fix to error handling

--- lib/database/base_service.dart
+++ lib/database/base_service.dart
@@ -157,16 +157,26 @@ class BaseService {
     return insertCounter;
   }
 
-  ErrorModel checkResultsForErrors(String responseBody)
-  {
+  ErrorModel checkResultsForErrors(String responseBody) {
     ErrorModel errorObj;
 
-    final List<dynamic> results = json.decode(responseBody);
-    for (List<dynamic> subResults in results) {
-      if (subResults.isNotEmpty) {
-        if (subResults[0].containsKey('errorId')) {
-          errorObj = ErrorModel.fromJson(subResults[0]);
+    // some results have multiple result sets, others have only one
+    if (responseBody.startsWith('[['))
+    {
+      // in this case, there are multiple result sets we need to check for errors
+      final List<dynamic> results = json.decode(responseBody);
+      for (List<dynamic> subResults in results) {
+        if (subResults.isNotEmpty) {
+          if (subResults[0].containsKey('errorId')) {
+            errorObj = ErrorModel.fromJson(subResults[0]);
+          }
         }
+      }
+    } else {
+      // in this case there is only one result set...
+      final dynamic results = json.decode(responseBody);
+      if (results[0].containsKey('errorId')) {
+        errorObj = ErrorModel.fromJson(results[0]);
       }
     }
     return errorObj;
