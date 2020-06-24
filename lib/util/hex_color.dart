5cf44c995210f94b9d89aea00af86a9926d77b38
james@sweetelectricity.com <james@jamesawhite.com>
2020-06-24 10:12:46 +0200

    1.0.14: Added HexColor class

---
+++ lib/util/hex_color.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.tryParse(hexColor, radix: 16) ?? int.tryParse('FF000000', radix: 16) ?? 0;
+  }
+}
